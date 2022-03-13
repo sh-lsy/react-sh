3ba3ab501e4e65a9ea963c8bf5a48dd27462e269
SH <673545134@qq.com>
2022-03-13 23:48:48 +0800

    路由 嵌套路由

--- src/views/router/index.jsx
+++ src/views/router/index.jsx
@@ -5,18 +5,26 @@ import About from './views/about'
 import {
   HashRouter,
   Route,
+  Redirect,
+  Switch
 } from "react-router-dom";
 export default function index() {
   return (
     <div>
       <HashRouter>
-        <Route path="/home" component={Home} />
-        <Route path="/person">
-          <Person></Person>
-        </Route>
-        <Route path="/about">
-          <About></About>
-        </Route>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/person">
+            <Person></Person>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          {/* 重定向 模糊匹配 */}
+          <Redirect from="/" to="/home" />
+          {/* 精确匹配  exact*/}
+          {/* <Redirect from="/" to="/home" exact/> */}
+        </Switch>
       </HashRouter>
     </div>
   )
--- src/views/router/views/home.jsx
+++ src/views/router/views/home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import {Redirect, Route, Switch} from 'react-router-dom'
+import HomeDec from './homeSec/homeDec'
+import News from './homeSec/news'
 export default function home() {
   return (
-    <div>home</div>
+    <div>
+      <p style={{height: '50px', background: 'skyBlue'}}>home</p>
+      {/* 嵌套路由 */}
+      <Switch>
+          <Route path="/home/homeDec" component={HomeDec}/>
+          <Route path="/home/news" component={News}/>
+          <Redirect from="/home" to="/home/news"/> 
+      </Switch>
+    </div>
   )
 }
